6323166fa7b0e80c32373d813fb3b1ad029ca167
Aleksandr Martyniuk <aleksandr_martyniuk@epam.com>
2020-08-10 20:15:24 +0400

    Added mocked data

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
+import json from '../mockData/categories.mock.json';
 
 export class App extends Component {
     render() {
+        console.log(json);
+
         return (
             <div>TTM application</div>
         )
